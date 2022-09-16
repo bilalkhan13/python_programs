47bcfaa28314d0884026c28dd689ce141c3f055d
bilalkhan13 <bilalahmad.khan@hotmail.com>
2022-09-16 22:57:58 +0500

    Test cases updated.

--- prime_number/test_module.py
+++ prime_number/test_module.py
@@ -3,10 +3,10 @@ import pytest
 from is_prime import is_prime
 
 test_cases = [
-pytest.param( "25", False, "Expected Input should be a prime number.", id = "test not a prime number."),
+pytest.param( "25", False, "Expected Input should be a prime number.", id = "test should be a prime number."),
 pytest.param( "5", True, "Expected Input should be a prime number.", id = "test a prime number."),
-pytest.param( "-2", False, "Expected Input should be a prime number.", id = "test not a prime number less than and equal to 1."),
-pytest.param( "A", False, "Expected Input should be a number.", id = "test not a prime number less than and equal to 1."),
+pytest.param( "-2", False, "Expected Input should be a prime number.", id = "test number less than and equal to 1."),
+pytest.param( "A", False, "Expected Input should be a number.", id = "test should be a prime number"),
 ]
 
 @pytest.mark.parametrize('arguments,expected_output,fail_message', test_cases)
